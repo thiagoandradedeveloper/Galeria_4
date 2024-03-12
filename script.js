2aa49628cc92a44378bf3c8fad11957a4d565c14
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-03-11 23:14:21 -0300

    Melhoramento

--- script.js
+++ script.js
@@ -5,29 +5,110 @@ window.onload = function(){
     let doc = document.documentElement;
     let draging = false, prevPageX;
     let container = document.querySelector("#container");
+    let conteinerButtons = document.querySelector("#containerBtns");
+    let contolerNumber = 0;
 
+    function avancar(){
+        container.append(imgs()[0]);
+        changerButton(1);
+    }
+    function voltar(){
+        container.prepend(imgs()[imgs().length-1]);
+        changerButton(-1);
+    }
+    
     function imgs(){
         return document.querySelectorAll("#container img");
     }
+
+    function changerButton(value){
+        
+        let btnSetar = document.querySelectorAll("#containerBtns span");
+        contolerNumber += value;
+        
+        if(contolerNumber < 0) contolerNumber = btnSetar.length - 1;
+        if(contolerNumber > (btnSetar.length - 1)) contolerNumber = 0;
+
+        btnSetar.forEach((element,index) => {
+            
+            if(index == contolerNumber){
+                element.style.background = "yellow";
+            } else {
+                element.style.background = "blue";
+            }
+
+            element.addEventListener("mousedown",(e)=>{
+
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                
+                if(index != contolerNumber){
+
+                    let pulos, re, av, cond = null;
+                    re = index - contolerNumber;
+                    
+                    if(re > 0){
+                        av = contolerNumber + (imgs().length - index);
+                    } 
+                    if(re < 0){
+                        av = index + (imgs().length - contolerNumber);
+                    }            
+    
+                    pulos = Math.min(Math.abs(re),Math.abs(av));
+    
+                    tamanho = imgs().length / 2;
+                    
+                    if(Math.abs(re) > tamanho){
+                        if(re < 0) cond = "a";
+                        if(re > 0) cond = "r";
+                    }
+                    if(Math.abs(re) < tamanho){
+                        if(re < 0) cond = "r";
+                        if(re > 0) cond = "a";                    
+                    }
+                    if(Math.abs(re) == tamanho)  cond = "a";
+    
+                    console.log(cond + pulos)
+    
+                    let contador = 0;
+                    let mover = setInterval(function(){
+                        if(cond == "a") avancar();
+                        else voltar();
+                        if(element.style.background == "yellow") clearInterval(mover)
+                    }, 1);
+                }                
+            })
+        })
+    }
     
     buttons[0].addEventListener("click",function(){
-        container.prepend(imgs()[imgs().length-1]);
+        voltar();
     })
 
     buttons[1].addEventListener("click",function(){
-        container.append(imgs()[0]);
+        avancar();
     })
 
-    imgs().forEach(element => {
+    imgs().forEach((element,index) => {
+
         element.addEventListener("mousedown",()=>{
             element.style.cursor = "grabbing";
         });
+
         element.addEventListener("mouseup",()=>{
             element.style.cursor = "pointer";
         });
+
         element.addEventListener("mouseleave",()=>{
             element.style.cursor = "pointer";
         });
+
+        let buttonControler = document.createElement("span");
+        buttonControler.id = index;
+        buttonControler.textContent = " ";
+        conteinerButtons.appendChild(buttonControler);
+
     })
     
     container.addEventListener("mousedown",function(e){
@@ -44,12 +125,12 @@ window.onload = function(){
             element.style.transition = "0.3s";
         })
 
-        let valorMinimo = 0;
+        let valorMinimo = 100;
 
         if(prevPageX > e.pageX){
 
             if((prevPageX - e.pageX) > valorMinimo){
-                container.append(imgs()[0]);    
+                avancar();
             }
             doc.style.setProperty("--position", "0px");
 
@@ -57,7 +138,7 @@ window.onload = function(){
         if(prevPageX < e.pageX){
 
             if((e.pageX - prevPageX) > valorMinimo){
-                container.prepend(imgs()[imgs().length-1]);
+                voltar();
             }
             doc.style.setProperty("--position", "0px");
 
@@ -81,5 +162,21 @@ window.onload = function(){
         let positionDiff = e.pageX - prevPageX;   
         doc.style.setProperty("--position", positionDiff + "px");
 
+    });
+    
+    changerButton(0);
+    
+    let automatico;
+    container.addEventListener("mouseout",function(){
+        automatico = setInterval(avancar,10000);
+    })
+    container.addEventListener("mouseover",function(){
+        clearInterval(automatico);
+    })
+
+    doc.addEventListener("keydown",function(e){
+        e.preventDefault();
+        if(e.key == "ArrowRight") avancar();
+        if(e.key == "ArrowLeft")  voltar();
     })
 }
